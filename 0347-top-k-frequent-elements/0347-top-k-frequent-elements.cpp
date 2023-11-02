090e6fbbde41bbfab3c4a3042fc5da9d005c896a
Mohammad Rabi Ul Hadi <42694156+morhadi@users.noreply.github.com>
2023-11-02 19:46:40 +0530

    Time: 8 ms (88.81%), Space: 14.6 MB (16.66%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,19 +1,33 @@
     class Solution {
 public:
     vector<int> topKFrequent(vector<int>& n, int k) {
-        map<int,int> m;
+        unordered_map<int,int> m;
         for(int i=0;i<n.size();i++){
             m[n[i]]++;
         }
-        vector<pair<int,int>> v;
-        for(auto it : m){
-            v.push_back(make_pair(it.first , it.second ));
+
+        // if (k==1) {
+        //     for (auto mit : m) {
+        //         hf
+        //     }
+        // }
+        // vector<pair<int,int>> v;
+        // for(auto it : m){
+        //     v.push_back(make_pair(it.first , it.second ));
+        // }
+        // sort( v.begin() , v.end() , [] ( pair<int, int>& a,  pair<int, int>& b) { return a.second > b.second; } ) ;
+
+        set<pair<int,int>> v;
+        for(auto mit : m){
+            v.insert(make_pair( mit.second , mit.first ));
         }
-        sort( v.begin() , v.end() , [] ( pair<int, int>& a,  pair<int, int>& b) { return a.second > b.second; } ) ;
-        v.erase(v.begin()+k ,v.end());
+        auto vit = v.end();
+        advance( vit , -k) ;
+        v.erase( v.begin() , vit );
+
         vector<int> v2;
         for(auto it2 : v){
-            v2.push_back(it2.first);
+            v2.push_back(it2.second);
         }
         return v2;
     }
