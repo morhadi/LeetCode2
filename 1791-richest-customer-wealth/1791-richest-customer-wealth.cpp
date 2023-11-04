824cf541372d8f4c7c90593473e5adbaed13408d
Mohammad Rabi Ul Hadi <42694156+morhadi@users.noreply.github.com>
2023-11-04 16:00:56 +0530

    Time: 3 ms (85.02%), Space: 8.1 MB (72.78%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.cpp
@@ -1,16 +1,40 @@
+void merge(int arr[] , int l , int mid ,int h){
+    int temp[h] ;
+    int i=l ,j=mid ;
+    while (i <= mid && j < h ) {
+        if(arr[l] < arr[mid+1]  ) {
+            temp[l] = arr[l];
+            l++;
+
+        }
+        // else {
+
+        // }
+    }
+}
+void dam(int arr[] , int l ,int h){
+    if(l==h) return ;
+    int mid = (l+h)/2;
+    dam(arr , l , mid);
+    dam(arr , mid + 1 , h);
+    merge(arr , l , mid ,h);
+}
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& a) {
         int ais = a[0].size() , as =a.size() ;
-        //int s[as];
-        set <int> s;
+        // int s[as];
+        // set <int> s;
+        int mw;
         for(int i=0 ;i<as; i++){
-            int wi=0;
+            int w=0;
             for(int j=0; j< ais ; j++){
-                wi+=a[i][j];
+                w+=a[i][j];
             }
-            s.insert(wi);
+            if(w>mw)
+            mw=w;
         }
-        return *prev(s.end());
+        // dam(s,0,ais);
+        return mw;
     }
 };
