7db732f40878c0314ef6befb4ce266506dedd4be
Mohammad Rabi Ul Hadi <42694156+morhadi@users.noreply.github.com>
2023-11-04 19:43:12 +0530

    Time: 7 ms (98.1%), Space: 17.3 MB (44.13%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int ns = nums.size();
+    int singleNumber(vector<int>& n) {
+        int ns = n.size();
         if (ns==1)
-        return nums[0];
+        return n[0];
+        if (ns==3)
+        return n[0]==n[1] ? n[2] : (n[1]==n[2] ? n[0] : n[1]);
         int a = 0;
         for(int i=0; i<ns ; i++){
-            a ^= nums[i];
+            a ^= n[i];
         }
         return a ;
     }
