24570484b123f66bb5588dd4df16bb4cb3896aa5
Mohammad Rabi Ul Hadi <42694156+morhadi@users.noreply.github.com>
2023-11-04 19:34:10 +0530

    Time: 20 ms (29.08%), Space: 17.2 MB (70.28%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -6,10 +6,11 @@ public:
         sort(n.begin() , n.end() );
         if ( n[0] < n[1] )
         return n[0] ;
-        for(int i = 1 ; i<n.size()-1 ; i++ ){
+        int ns=n.size();
+        for(int i = 1 ; i<ns ; i++ ){
             if (n[i-1] < n[i] && n[i] < n[i+1] ) 
             return n[i] ;
         }
-        return *n.rbegin();
+        return n[ns-1];
     }
 };
