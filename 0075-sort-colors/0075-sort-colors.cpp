050e116becfd1a31b4bc91b6efcf254149e5de4f
Mohammad Rabi Ul Hadi <42694156+morhadi@users.noreply.github.com>
2023-11-04 19:57:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (63.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& n) {
-        int ns=n.size(),n1=0,n2=0,n0=0;
-        if(ns==1)
-        return;
-        for(int i=0;i<ns;i++){
-            if(n[i]==0) n0++;
-            else if(n[i]==1) n1++;
-            else n2++;
-        }
-        vector<int> v(n0,0);
-        v.insert(v.end(),n1,1);
-        v.insert(v.end(),n2,2);
-        n=v;
-        v.clear();
+        // int ns=n.size(),n1=0,n2=0,n0=0;
+        // if(ns==1)
+        // return;
+        // for(int i=0;i<ns;i++){
+        //     if(n[i]==0) n0++;
+        //     else if(n[i]==1) n1++;
+        //     else n2++;
+        // }
+        // vector<int> v(n0,0);
+        // v.insert(v.end(),n1,1);
+        // v.insert(v.end(),n2,2);
+        // n=v;
+        // v.clear();
+        sort(n.begin(),n.end());
     }
 };
