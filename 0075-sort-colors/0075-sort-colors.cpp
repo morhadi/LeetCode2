016900d1d8b2d0271589b965748619cae5a739a3
Mohammad Rabi Ul Hadi <42694156+morhadi@users.noreply.github.com>
2023-11-05 19:36:21 +0530

    Time: 4 ms (33.11%), Space: 8.5 MB (64.87%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,20 +2,86 @@ class Solution {
 public:
     void sortColors(vector<int>& n) {
         int ns=n.size();
+
         if(ns==1)
         return;
-        int a[3] = {0,0,0};
-        for(int i=0;i<ns;i++){
-            a[n[i]]++;
+
+        if(ns==2){
+            if(n[0]<n[1])
+            return;
+            else 
+            swap(n[0],n[1]);
+            return;
         }
-        // vector<int> v(n0,0);
-        // v.insert(v.end(),n1,1);
-        // v.insert(v.end(),n2,2);
-        // n=v;
-        // v.clear();
-        n.clear();
-        n.insert(n.end(),a[0],0);
-        n.insert(n.end(),a[1],1);
-        n.insert(n.end(),a[2],2);
+
+        // int a[3] = {0,0,0};
+
+
+        // while (i<ns && l<h){
+        //     if(n[l]==0) {
+        //         l++ ;
+        //         continue;
+        //     }
+        //     if(n[h]==2) {
+        //         h-- ;
+        //         continue;
+        //     }
+        //     if(n[i]<n[h] && n[i]>n[l]){
+        //         h--;
+        //         l++;
+        //         continue;
+        //     }
+        //     if(n[i]>n[h]){
+        //         swap(n[i],n[h]);
+        //         i++;
+        //         h--;
+        //         continue;
+        //     }
+        //     if(n[i]<n[l]){
+        //         swap(n[i],n[l]);
+        //         l++;
+        //         i++;
+        //         continue;
+        //     }
+        //     if(n[i]>n[h] && n[i]<n[l]){
+        //         swap(n[l],n[h]);
+        //         h--;
+        //         l++;
+        //         continue;
+        //     }
+        //         i++;
+        // }
+
+        
+        int  l=0 , i=l  ,h=ns-1 ;
+        while(  i<=h ){
+            if(n[l]==0) {
+                l++ ;
+                i=l+1;
+                continue;
+            }
+            if(n[h]==2) {
+                h-- ;
+                continue;
+            }
+            if(n[l]>n[h] ) {
+                swap( n[l] , n[h]);
+                continue;
+            } // l=1 and h=1 
+            // i=l+1;
+            if(n[i]==0){
+                swap( n[l] , n[i]);
+            }
+            else if(n[i]==2){
+                swap( n[h] , n[i]);
+            }
+            else {
+                i++;
+            }
+        }
+        // n.clear();
+        // n.insert(n.end(),a[0],0);
+        // n.insert(n.end(),a[1],1);
+        // n.insert(n.end(),a[2],2);
     }
 };
